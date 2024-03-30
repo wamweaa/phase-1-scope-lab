c1aac72896a33af1792919ffb93d346807c58875
wamwea <patokinya12@gmail.com>
2024-03-31 00:00:50 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Write your solution in this file!
+var customerName = 'bob'
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase()
+}
+function setBestCustomer() {
+    return bestCustomer = 'not bob'
+}
+function overwriteBestCustomer() {
+    return bestCustomer = 'maybe bob'
+}
+function changeLeastFavoriteCustomer() {
+    const leastFavoriteCustomer ='bob'
+    return leastFavoriteCustomer='pato'
+}
